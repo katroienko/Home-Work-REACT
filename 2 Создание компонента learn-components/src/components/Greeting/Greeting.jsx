28ba3185f8d0a1c3e71ba2453e7d72b8a1316c5f
Roienko Kateryna <roienkokateryna@gmail.com>
2025-03-03 12:37:10 +0100

    fix2 hw2

--- 2 Создание компонента learn-components/src/components/Greeting/Greeting.jsx
+++
@@ -1,9 +0,0 @@
-// export default function Greeting({ name = "Гость" }) {
-//     return <h1>Привет, {name}!</h1>;
-//   }
-
-
-
-export default function Greeting(props) {
-    return <h1>Привет, {props.name}!</h1>;
-}
