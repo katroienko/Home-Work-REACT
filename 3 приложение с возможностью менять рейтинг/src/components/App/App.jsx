6bf3094617b2d432a5ac1442ba7e5846f7b19ade
Roienko Kateryna <roienkokateryna@gmail.com>
2025-03-05 13:59:45 +0100

    add 3 hw

---
+++ 3 приложение с возможностью менять рейтинг/src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import Rating from '../Rating/Rating'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="rating-container">
+      <h1>Поставьте свою оценку</h1>
+      <Rating />
+    </div>
+  );
+};
+export default App;
