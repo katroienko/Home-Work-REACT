77ec3e85a0b83975ae7d794dd8601bd61596516a
Roienko Kateryna <roienkokateryna@gmail.com>
2025-04-08 14:20:50 +0200

    fix 9 hw

--- 9_myForm/src/components/DynamicForm/DynamicForm.jsx
+++ 9_myForm/src/components/DynamicForm/DynamicForm.jsx
@@ -3,40 +3,61 @@ import { useForm } from 'react-hook-form';
 import styles from './DynamicForm.module.css';
 
 function DynamicForm() {
-    const { register, handleSubmit, reset, watch, formState: { errors } } = useForm();
-
-    const onSubmit = (values) => {
-        console.log(values);
-        reset();
-    };
+    const {
+        register,
+        handleSubmit,
+        reset,
+        watch,
+        formState: { errors },
+    } = useForm({ mode: 'onChange' });
 
     const firstField = watch('firstField');
 
+    const onSubmit = (data) => {
+        console.log(data);
+        reset();
+    };
+
     return (
         <div className={styles.wrapper}>
-            <form className={styles.form} onSubmit={handleSubmit(onSubmit)} >
+            <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
                 <div className={styles.input}>
-                    <label htmlFor="inputField">First field</label>
+                    <label htmlFor="inputField">First Field</label>
                     <input
-                        {...register("firstField", { required: 'First field is required' })}
+                        {...register('firstField', {
+                            required: 'First field is required',
+                            minLength: {
+                                value: 3,
+                                message: 'Minimum 3 characters required',
+                            },
+                        })}
                         type="text"
-                        placeholder='First field'
+                        placeholder="First field"
                         id="inputField"
                     />
-                    {errors.firstField && <p style={{ color: 'red' }}>{errors.firstField.message}</p>}
+                    {errors.firstField && (
+                        <div className={styles.error}>
+                            <p>{errors.firstField.message}</p>
+                        </div>
+                    )}
                 </div>
-                {firstField && (
+                {firstField && !errors.firstField && (
                     <div className={styles.input}>
-                        <label htmlFor="inputField2">Second field</label>
+                        <label htmlFor="inputField2">Second Field</label>
                         <input
-                            {...register('secondField', { required: 'Second field is required' })}
+                            {...register('secondField', {
+                                required: 'Second field is required',
+                            })}
                             type="text"
-                            placeholder='Second field'
+                            placeholder="Second field"
                             id="inputField2"
                         />
-                        {errors.secondField && <p style={{ color: 'red' }}>{errors.secondField.message}</p>}
+                        {errors.secondField && (
+                            <p style={{ color: 'red' }}>{errors.secondField.message}</p>
+                        )}
                     </div>
                 )}
+
                 <button type="submit">Submit</button>
             </form>
         </div>
