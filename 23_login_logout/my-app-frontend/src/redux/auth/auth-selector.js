2c3f6be7c1a6c39a50f7ffa26b9c8e9ad0a16da4
Kateryna Roienko <162887623+katroienko@users.noreply.github.com>
2025-05-25 15:31:08 +0200

    Delete 23_login_logout directory

--- 23_login_logout/my-app-frontend/src/redux/auth/auth-selector.js
+++
@@ -1,8 +0,0 @@
-export const selectAuthSuccess = store => store.auth.success;
-
-// export const selectIsLogin = store => {
-//     const { token, user } = store.auth;
-//     return Boolean(token) && Boolean(user);
-// }
-
-export const selectIsLogin = store => store.auth.success;
--- 23_login_logout/my-app-frontend/src/shared/lib/redux.js
+++
@@ -1,9 +0,0 @@
-export const pending = store => {
-    store.loading = true;
-    store.error = null;
-}
-
-export const rejected = (store, { payload }) => {
-    store.loading = false;
-    store.error = payload;
-}
